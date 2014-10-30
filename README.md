f2d2ed838ab44088fc9bc2e7738b6f4d8149a37c
Jared Kerim <jared.l.kerim@gmail.com>
2014-10-30 03:51:18 +0000

    Added key file logic, documentation

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-po2resx
-=======
+# po2resx 
 
-Convert PO translation files into .resx format
+**Convert PO translation files into Microsoft resx format**
+
+# Overview
+
+Automate the conversion of PO translation files, used in Django projects,
+into the resx XML format used by .NET projects.
+
+# Requirements
+
+* Python (2.6.5+, 2.7, 3.2, 3.3, 3.4)
+* Jinja 2.7.3
+* polib 1.0.5
+
+# Usage 
+
+## Create a key file
+
+In a Django PO file, translations are identified by the original text content
+found in the code or template, for example:
+
+    msgid "Your payment is complete."
+    msgstr "Votre paiement est terminé."
+
+However, in a .NET project, translations are identified with a unique identifier,
+so we must create a mapping from the unique identifier to the native language
+string.  To do that we create a key file:
+
+    python po2resx.py makekeys -pofile messages.po
+
+This will create a file called keys.json
+
+## Set unique identifiers 
+
+In the newly created keys.json you will find a JSON dictionary which maps
+the native language text of each entry in your PO file to the placeholder 
+"SET_KEY_HERE".
+
+Replace each instance of SET_KEY_HERE to the desired identifier for each
+translation string.
+
+## Convert your PO file to a resx
+
+Now that you've created a key file, you can use it to create the desired
+resx file:
+
+    python po2resx.py convert -pofile messages.po -keyfile keys.json -output messages.resx
--- po2resx.py
+++ po2resx.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+#
+import argparse
+import json
 import os
 from xml.sax.saxutils import escape
 
@@ -5,10 +9,11 @@ import jinja2
 import polib
 
 
+# Utilities
 def escape_xml(text):
-    """
-    Escape the following characters: < > & ' "
-    """
+    '''
+    Escape the following characters: < > & ' '
+    '''
     return escape(text, {
         '\'': '&apos;',
         '"': '&quot;',
@@ -23,8 +28,49 @@ def load_po(path):
     } for entry in pofile]
 
 
+def load_keys(path):
+    with open(path, 'rb') as jsonfile:
+        return json.loads(jsonfile.read())
+
+
+def write_file(content, path):
+    with open(path, 'wb') as output:
+        output.write(content.encode('utf8'))
+
+
+# Key Generation
+def get_keys(entries):
+    keys = dict([(entry['key'], 'SET_KEY_HERE') for entry in entries])
+    return json.dumps(
+        keys,
+        indent=4,
+    )
+
+
+def get_keyfile_path(input_path):
+    parent_path, input_file = os.path.split(input_path)
+    input_filename, ext = os.path.splitext(input_file)
+    output_filename = 'keys.json'
+    return os.path.join(parent_path, output_filename)
+
+
+def generate_keyfile(po_path):
+    entries = load_po(po_path)
+    keys = get_keys(entries)
+    keyfile_path = get_keyfile_path(po_path)
+    write_file(keys, keyfile_path)
+
+
+# PO Conversion
+def label_entries(entries, keys):
+    return [{
+        'key': keys[entry['key']],
+        'value': entry['value'],
+    } for entry in entries]
+
+
 def render_xml(entries):
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
     template = env.get_template('resx_template.xml')
     return template.render(entries=entries)
 
@@ -36,13 +82,38 @@ def get_output_path(input_path):
     return os.path.join(parent_path, output_filename)
 
 
-def write_file(content, path):
-    with open(path, 'wb') as output:
-        output.write(content.encode('utf8'))
-
-
-def convert_po_to_resx(po_path):
+def convert_po_to_resx(po_path, key_path, output):
     entries = load_po(po_path)
-    xml = render_xml(entries)
-    resx_path = get_output_path(po_path)
+    keys = load_keys(key_path)
+    labeled_entries = label_entries(entries, keys)
+    xml = render_xml(labeled_entries)
+    resx_path = output or get_output_path(po_path)
     write_file(xml, resx_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Convert a PO file to a Microsoft RESX file.')
+    parser.add_argument(
+        'command',
+        help='The command to execute.',
+        choices=('makekeys', 'convert'),
+    )
+    parser.add_argument(
+        '-pofile',
+        help='The path to the input PO file.',
+    )
+    parser.add_argument(
+        '-keyfile',
+        help='The path to the key file for conversion.',
+    )
+    parser.add_argument(
+        '-output',
+        help='An optional path to the output file.',
+    )
+    args = parser.parse_args()
+
+    if args.command == 'makekeys':
+        generate_keyfile(args.pofile)
+    elif args.command == 'convert':
+        convert_po_to_resx(args.pofile, args.keyfile, output=args.output)
